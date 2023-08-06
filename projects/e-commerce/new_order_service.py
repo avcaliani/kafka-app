db0730e82022cb3e158a6b91e4678effba7f5cfe
Anthony Caliani <avcaliani.it@gmail.com>
2023-08-05 21:38:28 -0300

    feat: Publishing another message.

--- projects/e-commerce/new_order_service.py
+++ projects/e-commerce/new_order_service.py
@@ -1,14 +1,14 @@
 import json
 from datetime import datetime
+from typing import Tuple
 from uuid import uuid4
 
+import names
 from kafka.producer.future import FutureRecordMetadata, RecordMetadata
 from utils.color import green
 
 from kafka import KafkaProducer
 
-KAFKA_TOPIC = "ECOMMERCE_NEW_ORDER"
-KAFKA_SERVERS = ["localhost:9092"]
 DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 
@@ -25,19 +25,34 @@ def showResponse(response: FutureRecordMetadata, message: str) -> None:
     )
 
 
+def new_order() -> Tuple[dict, dict]:
+    customer_id = str(uuid4())
+    customer_name = names.get_full_name()
+    email_msg = {
+        "customer_id": customer_id,
+        "customer_name": customer_name,
+        "customer_email": f"{customer_name.replace(' ', '.').lower()}@github.com",
+        "email_template": "template__new_order"
+    }
+    order_msg = {
+        "id": str(uuid4()),
+        "customer_id": customer_id,
+        "items": [],
+        "created_at": datetime.utcnow().strftime(DATETIME_FORMAT),
+    }
+    return email_msg, order_msg
+
+
 def run() -> None:
     print("🛒 New Order Service\nPress Ctrl+c to stop!")
     producer = KafkaProducer(
-        bootstrap_servers=KAFKA_SERVERS,
+        bootstrap_servers=["localhost:9092"],
         value_serializer=lambda v: json.dumps(v).encode("utf-8"),
     )
     try:
-        msg = {
-            "id": str(uuid4()),
-            "items": [],
-            "created_at": datetime.utcnow().strftime(DATETIME_FORMAT),
-        }
-        showResponse(producer.send(KAFKA_TOPIC, msg), msg)
+        email_msg, order_msg = new_order()
+        showResponse(producer.send(topic="ECOMMERCE_NEW_ORDER", value=order_msg), order_msg)
+        showResponse(producer.send(topic="ECOMMERCE_NEW_EMAIL", value=email_msg), email_msg)
     except (KeyboardInterrupt, SystemExit):
         producer.close()
         print("Bye bye!")
