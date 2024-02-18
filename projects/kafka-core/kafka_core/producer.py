54dfcfab09b18038ff29eb226b9aee530a440b1a
Anthony Caliani <avcaliani.it@gmail.com>
2024-02-18 10:00:04 -0300

    feat: Enhancing the log messages.

--- projects/kafka-core/kafka_core/producer.py
+++ projects/kafka-core/kafka_core/producer.py
@@ -20,14 +20,17 @@ def new_producer() -> KafkaProducer:
         producer.close()
 
 
-def show_sent_message(response: FutureRecordMetadata, message: str) -> None:
+def show_sent_message(
+    response: FutureRecordMetadata, message: str, show_message: bool = False
+) -> None:
     metadata: RecordMetadata = response.get()
     timestamp = datetime.utcfromtimestamp(metadata.timestamp / 1000)
     print(
         f"📦 "
-        f"{green('Topic')}: {metadata.topic} | "
-        f"{green('Partition')}: {metadata.partition} | "
-        f"{green('Offset')}: {metadata.offset} | "
-        f"{green('Timestamp')}: {timestamp.strftime(DATETIME_FORMAT)} | "
-        f"{green('Message')}: {message}"
+        f"{green('T')}: {metadata.topic} | "
+        f"{green('P')}: {metadata.partition} | "
+        f"{green('O')}: {metadata.offset} | "
+        f"{green('TS')}: {timestamp.strftime(DATETIME_FORMAT)}"
     )
+    if show_message:
+        print(f"{green('Message')}: {message}")
