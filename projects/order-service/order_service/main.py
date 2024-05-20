f26669918771c5a104dda7de6295d3ad534c9723
Anthony Caliani <avcaliani.it@gmail.com>
2024-05-19 21:54:53 -0300

    feat: Turning order service into an RESTful service.

--- projects/order-service/order_service/main.py
+++ projects/order-service/order_service/main.py
@@ -1,58 +1,31 @@
-from datetime import datetime
-from random import randint
-from time import sleep
-from typing import Tuple
-from uuid import uuid4
+from fastapi import FastAPI, status
+from kafka_core.producer import get_instance, show_sent_message
+from order_service.models import Order
 
-import names
-from kafka_core.common import DATETIME_FORMAT
-from kafka_core.producer import new_producer, show_sent_message
+app = FastAPI()
+kafka = get_instance()
 
 
-def new_order() -> Tuple[dict, dict]:
-    customer_name = names.get_full_name()
-    customer_email = f"{customer_name.replace(' ', '.').lower()}@github.com"
-    email_msg = {
-        "customer_name": customer_name,
-        "customer_email": customer_email,
+def _get_email_msg(order: Order) -> dict:
+    return {
+        "customer_email": order.customer_email,
         "email_template": "template__new_order",
     }
-    order_msg = {
-        "id": str(uuid4()),
-        "customer_name": customer_name,
-        "customer_email": customer_email,
-        "items": [],
-        "created_at": datetime.utcnow().strftime(DATETIME_FORMAT),
-    }
-    return email_msg, order_msg
 
 
-def run() -> None:
-    n_messages = randint(10, 1000)
-    print(f"Messages to be sent 👉 {n_messages}")
-    with new_producer() as producer:
-        for _ in range(0, n_messages):
-            email_msg, order_msg = new_order()
-            customer_email = bytes(order_msg["customer_email"], "utf-8")
-            show_sent_message(
-                producer.send(
-                    topic="ECOMMERCE_NEW_ORDER", key=customer_email, value=order_msg
-                ),
-                order_msg,
-            )
-            show_sent_message(
-                producer.send(
-                    topic="ECOMMERCE_NEW_EMAIL", key=customer_email, value=email_msg
-                ),
-                email_msg,
-            )
-            sleep(randint(0, 10) / 10)
-        print(f"\n{n_messages} messages sent ✅")
-
-
-if __name__ == "__main__":
-    print("📦 NEW ORDER SERVICE\nPress Ctrl+c to stop!")
-    try:
-        run()
-    except (KeyboardInterrupt, SystemExit):
-        print("Bye bye!")
+@app.post("/order", status_code=status.HTTP_201_CREATED)
+async def new_order(order: Order):
+    customer_email = bytes(order.customer_email, "utf-8")
+    show_sent_message(
+        kafka.send(
+            topic="ECOMMERCE_NEW_ORDER",
+            key=customer_email,
+            value=order.model_dump(mode="json"),
+        )
+    )
+    show_sent_message(
+        kafka.send(
+            topic="ECOMMERCE_NEW_EMAIL", key=customer_email, value=_get_email_msg(order)
+        )
+    )
+    return order
